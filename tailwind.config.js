c309c2eb923045873ad82c7b3e9deee99b1f0fda
Abdul Kader <abdulkader.69k@gmail.com>
2022-03-20 00:40:29 +0600

    Extending Tailwind with reusable third-party plugins.

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,5 +6,10 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/line-clamp'),
+    require('@tailwindcss/aspect-ratio'),
+  ],
 }
